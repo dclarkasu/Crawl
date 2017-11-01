f5342c3bc4adafcb4b168bd32b9e6d9975ad2480
dclarkasu <dclark205@gmail.com>
2017-11-01 11:17:30 -0600

    DB refactor

--- crawl.sql
+++ crawl.sql
@@ -1,14 +1,41 @@
 -- MySQL Workbench Forward Engineering
-DROP DATABASE IF EXISTS `crawldb`;
-CREATE DATABASE `crawldb`;
-USE `crawldb`;
+
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
 -- Schema crawldb
 -- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `crawldb` ;
+
+-- -----------------------------------------------------
+-- Schema crawldb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `crawldb` DEFAULT CHARACTER SET utf8 ;
+USE `crawldb` ;
+
+-- -----------------------------------------------------
+-- Table `address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `address` ;
+
+CREATE TABLE IF NOT EXISTS `address` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `long` DECIMAL(10,4) NULL DEFAULT NULL,
+  `lat` DECIMAL(10,4) NULL DEFAULT NULL,
+  `street` VARCHAR(255) NULL DEFAULT NULL,
+  `street2` VARCHAR(255) NULL DEFAULT NULL,
+  `city` VARCHAR(45) NULL DEFAULT NULL,
+  `state` VARCHAR(2) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
 
 -- -----------------------------------------------------
 -- Table `contact`
@@ -16,12 +43,13 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 DROP TABLE IF EXISTS `contact` ;
 
 CREATE TABLE IF NOT EXISTS `contact` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `phone_number` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `phone_number` VARCHAR(45) NULL DEFAULT NULL,
+  `email` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -30,10 +58,10 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `user` ;
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `first_name` VARCHAR(45) NOT NULL,
   `last_name` VARCHAR(45) NOT NULL,
-  `contact_id` INT UNSIGNED NULL,
+  `contact_id` INT(10) UNSIGNED NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
   INDEX `fk_user_contact_idx` (`contact_id` ASC),
@@ -42,78 +70,8 @@ CREATE TABLE IF NOT EXISTS `user` (
     REFERENCES `contact` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `login`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `login` ;
-
-CREATE TABLE IF NOT EXISTS `login` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `user_name` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `user_id` INT UNSIGNED NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  UNIQUE INDEX `user_name_UNIQUE` (`user_name` ASC),
-  INDEX `fk_login_user_idx` (`user_id` ASC),
-  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC),
-  CONSTRAINT `fk_login_user`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `address`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `address` ;
-
-CREATE TABLE IF NOT EXISTS `address` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `long` DECIMAL(10,4) NULL,
-  `lat` DECIMAL(10,4) NULL,
-  `street` VARCHAR(255) NULL,
-  `street2` VARCHAR(255) NULL,
-  `city` VARCHAR(45) NULL,
-  `state` VARCHAR(2) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `venue`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `venue` ;
-
-CREATE TABLE IF NOT EXISTS `venue` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(500) NULL,
-  `hours` VARCHAR(45) NULL,
-  `rating_id` INT UNSIGNED NULL,
-  `address_id` INT UNSIGNED NOT NULL,
-  `contact_id` INT UNSIGNED NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `fk_venue_contact_idx` (`contact_id` ASC),
-  INDEX `fk_venue_address_idx` (`address_id` ASC),
-  CONSTRAINT `fk_venue_contact`
-    FOREIGN KEY (`contact_id`)
-    REFERENCES `contact` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_venue_address`
-    FOREIGN KEY (`address_id`)
-    REFERENCES `address` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -122,9 +80,9 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `group` ;
 
 CREATE TABLE IF NOT EXISTS `group` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  `admin_id` INT UNSIGNED NULL,
+  `admin_id` INT(10) UNSIGNED NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
   INDEX `fk_group_user_idx` (`admin_id` ASC),
@@ -133,20 +91,22 @@ CREATE TABLE IF NOT EXISTS `group` (
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `routes`
+-- Table `route`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `routes` ;
+DROP TABLE IF EXISTS `route` ;
 
-CREATE TABLE IF NOT EXISTS `routes` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `route` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -155,50 +115,82 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `event` ;
 
 CREATE TABLE IF NOT EXISTS `event` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NULL,
-  `route_id` INT UNSIGNED NULL,
-  `group_id` INT UNSIGNED NULL,
-  `date` DATETIME NULL,
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL DEFAULT NULL,
+  `route_id` INT(10) UNSIGNED NULL DEFAULT NULL,
+  `group_id` INT(10) UNSIGNED NULL DEFAULT NULL,
+  `date` DATETIME NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC),
   INDEX `fk_event_route_idx` (`route_id` ASC),
   INDEX `fk_event_group_idx` (`group_id` ASC),
-  CONSTRAINT `fk_event_route`
-    FOREIGN KEY (`route_id`)
-    REFERENCES `routes` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_event_group`
     FOREIGN KEY (`group_id`)
     REFERENCES `group` (`id`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_event_route`
+    FOREIGN KEY (`route_id`)
+    REFERENCES `route` (`id`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
--- Table `user_group`
+-- Table `login`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `user_group` ;
+DROP TABLE IF EXISTS `login` ;
 
-CREATE TABLE IF NOT EXISTS `user_group` (
-  `user_id` INT UNSIGNED NOT NULL,
-  `group_id` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`user_id`, `group_id`),
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `user_name` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `user_id` INT(10) UNSIGNED NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  UNIQUE INDEX `user_name_UNIQUE` (`user_name` ASC),
   UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC),
-  UNIQUE INDEX `group_id_UNIQUE` (`group_id` ASC),
-  CONSTRAINT `fk_user_group_user`
+  INDEX `fk_login_user_idx` (`user_id` ASC),
+  CONSTRAINT `fk_login_user`
     FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `venue`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `venue` ;
+
+CREATE TABLE IF NOT EXISTS `venue` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(500) NULL DEFAULT NULL,
+  `hours` VARCHAR(45) NULL DEFAULT NULL,
+  `rating_id` INT(10) UNSIGNED NULL DEFAULT NULL,
+  `address_id` INT(10) UNSIGNED NOT NULL,
+  `contact_id` INT(10) UNSIGNED NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  INDEX `fk_venue_contact_idx` (`contact_id` ASC),
+  INDEX `fk_venue_address_idx` (`address_id` ASC),
+  CONSTRAINT `fk_venue_address`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `address` (`id`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_group_group`
-    FOREIGN KEY (`group_id`)
-    REFERENCES `group` (`id`)
+  CONSTRAINT `fk_venue_contact`
+    FOREIGN KEY (`contact_id`)
+    REFERENCES `contact` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
@@ -207,31 +199,75 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `route_venue` ;
 
 CREATE TABLE IF NOT EXISTS `route_venue` (
-  `route_id` INT UNSIGNED NOT NULL,
-  `venue_id` INT UNSIGNED NOT NULL,
+  `route_id` INT(10) UNSIGNED NOT NULL,
+  `venue_id` INT(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`route_id`, `venue_id`),
   UNIQUE INDEX `route_id_UNIQUE` (`route_id` ASC),
   UNIQUE INDEX `venue_id_UNIQUE` (`venue_id` ASC),
+  CONSTRAINT `fk_route_venue_rout`
+    FOREIGN KEY (`route_id`)
+    REFERENCES `route` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_route_venue_venue`
     FOREIGN KEY (`venue_id`)
     REFERENCES `venue` (`id`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `user_group`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user_group` ;
+
+CREATE TABLE IF NOT EXISTS `user_group` (
+  `user_id` INT(10) UNSIGNED NOT NULL,
+  `group_id` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`user_id`, `group_id`),
+  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC),
+  UNIQUE INDEX `group_id_UNIQUE` (`group_id` ASC),
+  CONSTRAINT `fk_user_group_group`
+    FOREIGN KEY (`group_id`)
+    REFERENCES `group` (`id`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_route_venue_rout`
-    FOREIGN KEY (`route_id`)
-    REFERENCES `routes` (`id`)
+  CONSTRAINT `fk_user_group_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+
+-- -----------------------------------------------------
+-- Table `post`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `post` ;
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `group_id` INT UNSIGNED NOT NULL,
+  `user_id` INT UNSIGNED NOT NULL,
+  `message` VARCHAR(500) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_post_user_idx` (`user_id` ASC),
+  INDEX `fk_post_group_idx` (`group_id` ASC),
+  CONSTRAINT `fk_post_user`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_post_group`
+    FOREIGN KEY (`group_id`)
+    REFERENCES `group` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-SET SQL_MODE = '';
-GRANT USAGE ON *.* TO crawluser@localhost;
- DROP USER crawluser@localhost;
-SET SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-CREATE USER 'crawluser'@'localhost' IDENTIFIED BY 'crawl';
-
-GRANT SELECT, INSERT, TRIGGER ON TABLE * TO 'crawluser'@'localhost';
-GRANT SELECT, INSERT, TRIGGER, UPDATE, DELETE ON TABLE * TO 'crawluser'@'localhost';
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
