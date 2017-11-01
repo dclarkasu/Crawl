4699d0c55a5801d78b02079676632933f124b7a2
dclarkasu <dclark205@gmail.com>
2017-11-01 16:50:25 -0600

    Contact Test

---
+++ CrawlJPA/test/tests/ContactTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import entities.Contact;
+
+public class ContactTest {
+	private EntityManagerFactory emf = null;
+	private EntityManager em = null;
+	Contact contact;
+
+	@Before
+	public void setUp() {
+		emf = Persistence.createEntityManagerFactory("CrawlPU");
+		em = emf.createEntityManager();
+	}
+
+	@After
+	public void tearDown() {
+		em.close();
+		emf.close();
+	}
+
+	@Test
+	public void smokeTest() {
+		assertEquals(true, true);
+	}
+
+	@Test
+	public void user_Connected_To_DB() {
+		contact = em.find(Contact.class, 1);
+		assertEquals(contact.getPhoneNumber(), "555-555-5555");
+		assertEquals(contact.getEmail(), "bill@gmail.com");
+	
+	}
+}
