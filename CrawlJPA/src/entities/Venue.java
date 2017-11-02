ff18cb92efd6797f429de6a65bc60f38cced185e
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-11-02 17:24:47 -0600

    finished everything in venue controller and dao

--- CrawlJPA/src/entities/Venue.java
+++ CrawlJPA/src/entities/Venue.java
@@ -12,9 +12,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity
 public class Venue {
 
