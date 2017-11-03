64642c263015ff38b19ee309c98624104aae69c4
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-11-03 10:00:24 -0600

    started angular stuff and fixed some database stuff

---
+++ CrawlREST/WebContent/app/authModule/auth.service.js
@@ -0,0 +1,73 @@
+angular.module('authModule')
+  .factory('authService', function($http, $cookies) {
+    var service = {};
+
+    var saveToken = function(user) {
+    		$cookies.put('userId', user.id);
+    		$cookies.put('email', user.email);
+    }
+
+    service.getToken = function() {
+      return {
+    	  	userId: $cookies.get('userId'),
+    	  	email: $cookies.get('email')
+      }
+    }
+
+    var removeToken = function() {
+    		$cookies.remove('userId');
+    		$cookies.remove('email');
+    }
+
+    service.login = function(user) {
+    		console.log(user);
+    		return $http({
+		      method : 'POST',
+		      url : 'rest/auth/login/',
+		    	  headers : {
+			          'Content-Type' : 'application/json'
+			        },
+			  data : user
+		    })
+		    .then(function(res){
+		    		saveToken(res.data);
+		    		return res;
+		    }).
+		    catch(function(err){
+		    		console.log(err);
+		    });
+    }
+
+    service.register = function(user) {
+    		console.log(user);
+    		return $http({
+		      method : 'POST',
+		      url : 'rest/auth/register/',
+		    	  headers : {
+			          'Content-Type' : 'application/json'
+			        },
+			  data : user
+		    })
+		    .then(function(res){
+		    		saveToken(res.data);
+		    		return res;
+		    });
+    }
+
+    service.logout = function() {
+    		return $http({
+		      method : 'POST',
+		      url : 'api/auth/logout/',
+		    	  headers : {
+			          'Content-Type' : 'application/json'
+			        },
+			  data : null
+		    })
+		    .then(function(res){
+		    		removeToken();
+		    		return res;
+		    });
+    }
+
+    return service;
+  })
