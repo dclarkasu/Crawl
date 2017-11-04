9443f21d45041caf395cfdc2204601f79ec6317a
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-11-04 14:22:16 -0600

    made some minor changes to app.module.js

--- CrawlREST/WebContent/app/appModule/app.module.js
+++ CrawlREST/WebContent/app/appModule/app.module.js
@@ -1,29 +1,23 @@
+
 angular.module('appModule', ['ngRoute','ngCookies','authModule'])
 .config(function($routeProvider){
 	$routeProvider
 	.when('/',{
-		template: '<home></home>'
+		template :'<home></home>'
 	})
 	.when('/about',{
-		template: '<about></about>'
+		template :'<about></about>'
 	})
-	.when('/login', {
-		template : `
-		<login></login>
-	`
+	.when('/login',{
+		template :'<login></login>'
 	})
-	.when('/register', {
-		template : `
-		<register></register>
-	`
+	.when('/resister',{
+		template : '<register></register>'
 	})
 	.when('/venue',{
-		template : `
-	    <venue></venue>
-	    `
-	.when('/group', {
-		template : `
-		<group></group>
-	`
+		template : '<venue></venue>'
+	})
+	.when('/group',{
+		template : '<group></group>'
 	})
 });
