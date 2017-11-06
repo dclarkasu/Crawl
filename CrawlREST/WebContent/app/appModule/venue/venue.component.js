02403fea78c2908a619952f043978ff0224e2444
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-11-05 18:23:13 -0700

    stuff

--- CrawlREST/WebContent/app/appModule/venue/venue.component.js
+++ CrawlREST/WebContent/app/appModule/venue/venue.component.js
@@ -6,12 +6,24 @@ angular.module('appModule')
 		var vm = this;
 		vm.venueList = [];
 		vm.venue = null;
+		vm.venueId = $routeParams.vid;
 		load();
 		loadVenue();
 		function load(){
 			venueService.indexVenue()
 			.then(function(res){
 				vm.venueList = res.data;
+			})
+			.catch(function(err){
+				console.log(err);
+			});
+		}
+		function loadVenue(){
+			console.log('in loadVenue');
+			venueService.showVenue($routeParams.vid)
+			.then(function(res){
+				console.log('in then');
+				console.log(res.data);
 			})
 			.catch(function(err){
 				console.log(err);
