a54acb838d45b1eb56030d39f595a5887b2d667d
Erik Ernst <erikrobeyernst@Eriks-MacBook-Pro.local>
2017-11-06 12:06:26 -0700

    images

--- CrawlREST/WebContent/app/appModule/route/route.component.js
+++ CrawlREST/WebContent/app/appModule/route/route.component.js
@@ -5,29 +5,38 @@ angular.module('appModule').component('route', {
 		var vm = this;
 
 		vm.route = null;
+		vm.venues = null;
 
-		//Behaviors
-		vm.groupsByUser = function() {
-			groupService.indexUserGroups()
-			.then(function(res){
-				console.log(res.data);
-				vm.groupList = res.data;
-			})
-		};
 
 		vm.loadRoute = function() {
 			var promise = routeService.showRoute();
+			
 			promise.then(function(res){
 				console.log(res);
 				console.log('in promise');
+				
 				vm.route = res.data;
+			
 			}).catch(function(err){
 				console.log(err);
 			});
 		}
 
+		vm.loadVenues = function() {
+			var promise = routeService.indexVenues();
+			console.log('in promise 2');
+			promise.then(function(res){
+				
+				vm.venues = res.data;
+			}).catch(function(err){
+				console.log(err);
+			});
+		}
+		
 		vm.loadRoute();
 		console.log('vm.route: ' + vm.route);
+		vm.loadVenues();
+		console.log('vm.venues: ' + vm.venues);
 
 		vm.loadMembers = function() {
 			console.log('in load members');
